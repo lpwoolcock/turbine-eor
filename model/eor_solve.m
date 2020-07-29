c4aecabb34b43b61c84f5a411489155dccea2d39
lpwoolcock <lpwoolcock@student.unimelb.edu.au>
2020-07-29 20:05:14 +1000

    - Fix bug in EOR code - Make eor_solve use actual inverse instead of pinv

--- model/eor_solve.m
+++ model/eor_solve.m
@@ -6,11 +6,10 @@ function [Pi, Gamma] = eor_solve(A,B,Ew,Ce,Deu,Dew,S)
     s = size(S, 1); % no. exosystem states
     
     A1 = [A B; Ce Deu];
-    A1_dagger = pinv(A1);
     A2 = [eye(n) zeros(n,m); zeros(q,n+m)];
     
     coder.extrinsic('dlyap');
-    X = dlyap(A1_dagger*A2, S, -A1_dagger*[Ew;Dew]);
+    X = dlyap(A1\A2, S, -A1\[Ew;Dew]);
     Pi = X(1:n,1:s);
     Gamma = X(n+1:n+m,1:s);
 end
